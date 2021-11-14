510e601aa79dd2de07670abb9189b594fed0a2dc
Chethram Ramoutar <cramoutar1337@gmail.com>
2021-11-13 20:38:36 -0500

    added features to put debits and credits into main

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
 class Credits extends Component {
     render() {
       return (
           <div>
-            
+            <h1>Credit Page</h1>
+            <div> Credits: {this.props.credits} </div>
+            <Link to="/">Return to Home</Link>
           </div>
       );
     }
